f29890b82940e29cf5be2a6e37599c6ee12a6591
Sarita <atanacio.sarita@pucp.pe>
2020-05-18 19:41:13 -0500

    avance1

--- src/main/java/sw2/lab5/Config/WebSecurityConfig.java
+++ src/main/java/sw2/lab5/Config/WebSecurityConfig.java
@@ -18,15 +18,36 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception  {
-        http.formLogin();
+
+        http.formLogin()
+                .loginPage("/loginForm") //ruta del formulario de login.html
+                .loginProcessingUrl("/processLogin") //el boton del proceso submit del login
+                .defaultSuccessUrl("/redirectByRole", true);
+
+        http.logout()
+                .logoutSuccessUrl("/");
+
         http.authorizeRequests()
-            .anyRequest().permitAll();
+                //Protege ruta /employee y una debajo de ella. Sólo pueden verlo admin y logistica
+                .antMatchers("/user/list").hasAuthority("admin")
+                .antMatchers("/user", "/user/edit").hasAuthority("admin")
+                .antMatchers("/post/delete").hasAuthority("admin")
+                .antMatchers("/post/create").hasAuthority("admin")
+                .antMatchers("/post/**").authenticated()
+                .antMatchers("/user/**").authenticated()
+                .anyRequest().permitAll();
     }
+    @Autowired
+    DataSource datasource;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication()
-            .dataSource(dataSource)
-            .passwordEncoder(new BCryptPasswordEncoder());
+                .dataSource(datasource)
+                .passwordEncoder(new BCryptPasswordEncoder())
+                //Obligatorio: identificador de validacion , password , estado logico del usuario
+                .usersByUsernameQuery("SELECT user.email, user.password, user.active FROM user where email =? ")
+                //roles y email
+                .authoritiesByUsernameQuery("SELECT user.email, role.role_name FROM user INNER JOIN role ON (user.id_role = role.id_role) WHERE user.email = ? and user.active = 1");
     }
 }
